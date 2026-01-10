e90550acc1a59aa176e9b8c77b955f8420c30520
Suniil Ghate <sunil.82ghate@gmail.com>
2026-01-10 21:17:10 +0530

    App Structure and folders

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <div className="container mt-5">
+      <h2>Dashboard</h2>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div className="container mt-5">
+      <h2>Login Page</h2>
+    </div>
+  )
+}
+
+export default Login
